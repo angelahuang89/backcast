00b814d030b1527e70f503d0b89dce77c0051221
angelahuang89 <angelahuang89@gmail.com>
2017-10-23 20:58:34 -0700

    Work on backcast

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,5 +1,11 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    // your code here
+    // this.videos = new Videos(window.exampleVideoData);
+    this.render();
+    // this.collection.on('change', this.render, this);
+  },
 
   render: function() {
     this.$el.children().detach();
